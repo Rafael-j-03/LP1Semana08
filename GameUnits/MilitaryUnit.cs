8045233de07921320c1794c943eafde1fa19716d
Rafael José <ra.david@hotmail.com>
2023-05-03 22:23:17 +0100

    Add constructor to MilitaryUnit

--- GameUnits/MilitaryUnit.cs
+++ GameUnits/MilitaryUnit.cs
@@ -12,6 +12,12 @@ namespace GameUnits
         public override int Health => base.Health + XP;
         public override float Cost => AttackPower + XP;
 
+        // Constructor
+        public MilitaryUnit(int movement, int health, int attackPower) : base(movement, health)
+        {
+            AttackPower = attackPower;
+        }
+
         public void Attack (Unit u)
         {
             XP++;
